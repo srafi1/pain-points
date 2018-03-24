7c719b6d85336ae775546cd2bad275f14a5cfd40
jerry1ye10 <jerry1ye10@gmail.com>
2018-03-24 17:56:23 -0400

    commented out bugged code

--- models.py
+++ models.py
@@ -7,11 +7,11 @@ class User(db.Model):
     name = db.Column(db.String(30))
     skills = db.Column(db.String(300))
     posts = db.relationship('Posts',backref='user', lazy=True)
-class Posts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
-    content = db.Column(db.text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#*class Posts(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    title = db.Column(db.String(80))
+#    content = db.Column(db.text)
+#    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, username, password):
         self.username = username
