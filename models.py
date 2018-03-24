f65cb6ba34e81e823f98beef66ec347ef96d091e
jerry1ye10 <jerry1ye10@gmail.com>
2018-03-24 17:17:12 -0400

    Added posts model with one to many relationship

--- models.py
+++ models.py
@@ -6,6 +6,12 @@ class User(db.Model):
     password = db.Column(db.String(80))
     name = db.Column(db.String(30))
     skills = db.Column(db.String(300))
+    posts = db.relationship('Posts',backref='user', lazy=True)
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    content - db.Column(db.text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, username, password):
         self.username = username
