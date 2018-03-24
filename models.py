fb42b13be98c53351ce11feb87ab2a2bd85b65c0
srafi1 <shakilrafi0@gmail.com>
2018-03-24 16:27:39 -0400

    login and stuff

---
+++ models.py
@@ -0,0 +1,16 @@
+from main import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
+    name = db.Column(db.String(30))
+    skills = db.Column(db.String(300))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.name = ''
+
+    def __repr__(self):
+        return '<User %r>' % self.username
