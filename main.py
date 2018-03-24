5f6ab74447f0a1a51bbf22dea46d2ab964878ea0
darthbeep <darthbeep@gmail.com>
2018-03-24 16:11:31 -0400

    feed looks tolerable

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def home():
         {
             'name': "Shaina",
             'likes': 0,
+            'id':0,
             'idea': "Ad ea velit deserunt irure esse minim proident ut adipisicing irure ex adipisicing consectetur et ipsum labore deserunt. Est anim enim amet cupidatat ad consequat irure ad do consectetur quis dolor nostrud est qui. Tempor amet sint culpa et culpa duis sint esse laborum duis eiusmod. Esse enim proident enim eu aliquip do ea anim culpa tempor. Qui exercitation quis fugiat enim amet mollit officia ullamco. Est sit cillum tempor sit excepteur irure anim ad occaecat.",
             'commentlist': [
                 {'user': 'comm', 'message':'idk'},
@@ -18,6 +19,7 @@ def home():
         {
             'name': "Shakil",
             'likes': 0,
+            'id':1,
             'idea': "thing2",
             'commentlist': [
                 {'user': 'comm3', 'message':'idk and'},
